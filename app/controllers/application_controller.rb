e0744acbb3d5bdd8ade9264df5790d88e9ce82d1
Roshan Basu <roshanbasu7@gmail.com>
2024-04-10 01:43:22 +0530

    Controller implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
+require "#{Rails.root}/lib/errors/custom_api_error.rb"
+
 class ApplicationController < ActionController::Base
+
+  include Errors
+
+  rescue_from StandardError, with: :render_standard_error
+  #for any custom error which could be having 400, 401, 404, 422 error codes, it should be raised using CustomApiError in the application
+  rescue_from Errors::CustomApiError, with: :render_error_api_response
+
+  private
+
+  def render_error_api_response(error)
+    #byebug
+    render json: {
+      error: {
+        title: error.title,
+        message: error.message
+      }
+    }, status: error.code
+  end
+
+  def render_standard_error(error)
+    #byebug
+    render_error_api_response(CustomApiError.new(error.message, 500))
+  end
+
 end
